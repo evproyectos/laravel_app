71fdd1da057ce18d8a8315b6b6f61bac8694277d
Emanuel Vargas <evproyectosp@gmail.com>
2025-08-06 21:48:00 -0600

    Octavo2 commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,24 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Vite;
+use Illuminate\Support\ServiceProvider;
 
-public function boot()
+class AppServiceProvider extends ServiceProvider
 {
-    if (env('APP_ENV') !== 'local') {
-        URL::forceScheme('https');
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Vite::prefetch(concurrency: 3);
     }
 }
